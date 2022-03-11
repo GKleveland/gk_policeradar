22f41b1b78145c794074ad7629a54a48212e9aee
GKleveland <69856514+GKleveland@users.noreply.github.com>
2022-03-11 18:39:25 +0100

    Add files via upload

---
+++ gk_policeradar/fxmanifest.lua
@@ -0,0 +1,15 @@
+fx_version "bodacious"
+games {"gta5"}
+
+client_script {
+    '@mysql-async/lib/MySQL.lua',
+	'@es_extended/locale.lua',
+    "radar.lua"
+}
+
+--server_script {
+--    '@es_extended/locale.lua',
+--    "server/main.lua",
+--   'locales/en.lua',
+--    "config.lua"
+--}
